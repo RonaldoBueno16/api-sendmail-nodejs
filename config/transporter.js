834ba2f9b59e30a4bcb599812a9cd7d9499dc876
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-10-04 14:13:17 -0300

    Atualizando env_example

--- config/transporter.js
+++ config/transporter.js
@@ -4,7 +4,6 @@ const {google} = require("googleapis");
 require ('dotenv').config();
 
 module.exports = async () => {
-
     try {
         const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
         oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
