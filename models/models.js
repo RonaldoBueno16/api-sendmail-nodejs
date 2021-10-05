4d3fd40b0b05e06c978e8c5afc0169d60e56428c
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-10-04 22:47:25 -0300

    Adicionando conversão de JSON para SAMP(Pawn)

--- models/models.js
+++ models/models.js
@@ -4,44 +4,60 @@ const v = new Validator();
 
 class mail {
     async sendMaail(data, transporter, res) {
-          const schema = {
-            type: "object",
-            properties: {
-              from: {"type": "string"},
-              to: {"type": "string"},
-              subject: {"type": "string"},
-              text: {"type": "string"},
-              html: {"type": "string"},
-            },
-            required: ['to', 'subject']
-          };
-           
-          if(v.validate(data, schema).valid) {
-            
-            const mailOptions = {
-              from: `${process.env.EMAIL_NAME} <${process.env.EMAIL}>`,
-              to: data.to,
-              subject: data.subject,
-              text: data.text,
-              html: data.html
+
+        try {
+            const teste = Object.getOwnPropertyNames(data);
+            console.log(teste);
+            if(teste.length == 1) { //Converter em JSON (SAMP)
+                data = JSON.parse(teste[0]);
+                console.log("oiii");
+                console.log(data);
             }
-
-            const result = await transporter.sendMail(mailOptions);
-
-            res.status(200).json({
-              status: 'sucess',
-              response: 'Email enviado com sucesso',
-              data: result
-            });
-          }
-          else {
-              res.status(400).json({"status": "failed", "response": "JSON inválido", "json_example:": {
-                  to: "Destinatario",
-                  subject: "Assunto",
-                  text: "Corpo",
-                  html: "HTML (opcional)"
-              }});
-          }
+    
+            const schema = {
+                type: "object",
+                properties: {
+                from: {"type": "string"},
+                to: {"type": "string"},
+                subject: {"type": "string"},
+                text: {"type": "string"},
+                html: {"type": "string"},
+                },
+                required: ['to', 'subject']
+            };
+            
+            if(v.validate(data, schema).valid) {
+                
+                const mailOptions = {
+                from: `${process.env.EMAIL_NAME} <${process.env.EMAIL}>`,
+                to: data.to,
+                subject: data.subject,
+                text: data.text,
+                html: data.html
+                }
+    
+                const result = await transporter.sendMail(mailOptions);
+    
+                res.status(200).json({
+                    status: 'sucess',
+                    response: 'Email enviado com sucesso',
+                    data: result
+                });
+            }
+            else {
+                res.status(400).json({"status": "failed", "response": "JSON inválido", "json_example:": {
+                    to: "Destinatario",
+                    subject: "Assunto",
+                    text: "Corpo",
+                    html: "HTML (opcional)"
+                }});
+            }
+        }
+        catch(error) {
+            console.log(error);
+            res.status(400).json(error);
+        }
+        
     }
 }
 module.exports = new mail;
