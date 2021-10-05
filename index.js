397bd224b385c672af502f51649e09aa4e5d2e13
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-10-05 19:29:06 -0300

    Adicionando conexão com mysql

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const customExpress = require("./config/customExpress");
 const cors = require("cors");
+const conexao = require("./infraestrutura/connect");
 
 const port = process.env.PORT || 8080;
+
+
+
+conexao.getConnection((error, connection) => {
+    if(error) {
+        console.log("NÃO CONECTOU");
+    }
+    else {
+        console.log("CONECTOU");
+    }
+})
 
 const app = customExpress();
 app.use(cors());
---
+++ infraestrutura/connect.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+require ('dotenv').config();
+
+const conexao = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+module.exports = conexao;
